e181a08e23e00bcb6b5c5ad8a7faac8e9eecf3f2
Ngoc Duc Nghiem <161023401+NghiemNgocDuc@users.noreply.github.com>
2026-01-11 20:37:40 +0700

    Update version5.c

--- version5.c
+++ version5.c
@@ -5,3 +5,36 @@
 #include <string.h>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
+
+#define INPUT_SIZE 784
+#define HIDDEN_SIZE 1024
+#define OUTPUT_SIZE 10
+#define TRAIN_SIZE 10000
+#define TEST_SIZE 10000
+#define BATCH_SIZE 32
+#define EPOCHS 10
+#define LEARNING_RATE 0.01
+
+#define CUDA_CHECK(call) \
+    do { \
+        cudaError_t error = call; \
+        if (error != cudaSuccess) { \
+            fprintf(stderr, "CUDA error at %s:%d: %s (%d)\n", __FILE__, __LINE__, \
+                    cudaGetErrorString(error), error); \
+            cudaDeviceReset(); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
+
+#define CUBLAS_CHECK(call) \
+    do { \
+        cublasStatus_t status = call; \
+        if (status != CUBLAS_STATUS_SUCCESS) { \
+            fprintf(stderr, "cuBLAS error at %s:%d: %d\n", __FILE__, __LINE__, status); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
+
+double get_time_diff(struct timespec start, struct timespec end) {
+    return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+}
